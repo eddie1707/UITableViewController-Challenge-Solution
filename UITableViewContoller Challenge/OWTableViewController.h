be2ec421b5a255dafc53dca64c9ebc10e1e7038f
Edwin Wiersma <Edwin@MacBook-Pro-van-Edwin.local>
2014-05-20 16:46:55 +0200

    solved the challenge

--- UITableViewContoller Challenge/OWTableViewController.h
+++ UITableViewContoller Challenge/OWTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  OWViewController.h
+//  OWTableViewController.h
 //  UITableViewContoller Challenge
 //
 //  Created by Edwin Wiersma on 20-05-14.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface OWViewController : UIViewController
+@interface OWTableViewController : UITableViewController
 
 @end
